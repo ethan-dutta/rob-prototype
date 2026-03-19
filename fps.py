12fbd539cc846de209d53e58328323bc97049cd0
ethan-dutta <2681401@schoolsnet.act.edu.au>
2026-03-19 18:02:43 +1100

    Create fps.py

---
+++ fps.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import cv2
+import datetime
+from threading import Thread
+
+
+class FPS:
+  def __init__(self):
+     self._start = None
+     self._end = None
+     self._NumFrames = 0
+
+
+  def start(self):
+     self._start = datetime.datetime.now()
+     return self
+ 
+  def stop(self):
+     self._end = datetime.datetime.now()
+
+
+  def update(self):
+     self._NumFrames += 1
+
+
+  def elapsed(self):
+     return (self._end - self._start).total_seconds()
+ 
+  def fps(self):
+     return self._NumFrames / self.elapsed()
+ 
+class WebcamVideoStream:
+   def __init__(self, src=0):
+       self.stream = cv2.VideoCapture(src)
+       (self.grabbed, self.frame) = self.stream.read()
+       self.stopped = False
+
+
+   def start(self):
+       Thread(target=self.update, args=()).start()
+       return self
+  
+   def update(self):
+       while True:
+           if self.stopped:
+               return
+           (self.grabbed, self.frame) = self.stream.read()
+
+
+   def read(self):
+       return self.frame
+  
+   def stop(self):
+       self.stopped = True
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100, help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1, help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
+
+
+
+
+
+print("[INFO] sampling frames from webcam...")
+stream = cv2.VideoCapture(0)
+fps = FPS().start()
+
+
+while fps._numFrames < args["num_frames"]:
+   (grabbed, frame) = stream.read()
+   frame = imutils.resize(frame, width=400)
+
+
+   if args["display"] > 0:
+       cv2.imshow("Frame", frame)
+       key = cv2.waitKey(1) & 0xFF
+
+
+   fps.update()
+
+
+fps.stop()
+print("[INFO] elapsed time: {: .2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {: .2f}".format(fps.fps()))
+
+
+stream.release()
+cv2.destroyAllWindows()
