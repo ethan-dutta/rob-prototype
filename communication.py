3eb383c7d4e6ed0d14167855d6b4a7c074e8237a
ethan-dutta <2681401@schoolsnet.act.edu.au>
2026-03-19 18:04:16 +1100

    Create communication.py

---
+++ communication.py
@@ -0,0 +1,15 @@
+import serial
+import time
+import random
+
+
+if __name__ == '__main__':
+   ser=serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+   ser.reset_input_buffer()
+
+
+   while True:
+       number = ser.write(b"Hello from Raspberry Pi!\n")
+       line= ser.readline().decode('utf-8').rstrip()
+       print(line)
+       time.sleep(1)
